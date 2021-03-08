75c85c9c9a849b8d682a6ff4e64f7d194bc5753e
Alex Fernandes <alex@labela.nl>
2021-03-08 13:49:15 +0100

    multilingual header

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,9 +60,7 @@ export const pageQuery = graphql`
         }
       }
     }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
+    allContentfulPerson(filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" }, node_locale: {eq: $locale} }) {
       edges {
         node {
           name
